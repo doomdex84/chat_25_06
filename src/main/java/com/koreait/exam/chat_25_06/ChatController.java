424099893c08ac8a0c9951cdfd5a48ebffda437e
doomdex84 <doomdex84@gmail.com>
2025-06-12 14:20:29 +0900

    messagesResponse record 생성2

--- src/main/java/com/koreait/exam/chat_25_06/ChatController.java
+++ src/main/java/com/koreait/exam/chat_25_06/ChatController.java
@@ -1,53 +1,51 @@
-
 package com.koreait.exam.chat_25_06;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@@ -14,21 +12,14 @@ public class ChatController {
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
 
     private List<ChatMessage> chatMessages = new ArrayList<>();
 
-    public record writeChatMessageResponse(long id, String authorName, String content) {
-
-
-    }
-
-//    @AllArgsConstructor
-//    @Getter
-//    public static class writeChatMessageRequest {
-//        private final String authorName;
-//        private final String content;
-//    }
 
     public record writeChatMessageRequest(String authorName, String content) {
     }
 
+    public record writeChatMessageResponse(long id, String authorName, String content) {
+
+    }
 
     @PostMapping("/writeMessage")
     @ResponseBody
     public RsData<writeChatMessageResponse> writeMessage(@RequestBody writeChatMessageRequest req) {
-        @@ -43,14 +34,18 @@ public RsData<writeChatMessageResponse> writeMessage(@RequestBody writeChatMessa
+        ChatMessage message = new ChatMessage(req.authorName, req.content);
+
+        chatMessages.add(message);
+
+        return new RsData<>(
+                "S-1",
+                "메세지가 작성됨",
+                new writeChatMessageResponse(message.getId(), message.getAuthorName(), message.getContent())
         );
     }
 
+    public record messagesResponse(List<ChatMessage> chatMessages, long count) {
 
-
-
+    }
 
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<List<ChatMessage>> showMessages() {
+    public RsData<messagesResponse> showMessages() {
 
         return new RsData<>(
                 "S-1",
                 "성공",
-                chatMessages
+                new messagesResponse(chatMessages, chatMessages.size())
         );
     }
 }
